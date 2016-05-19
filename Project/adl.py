a898ffe76bd38666d96d051a703463a3a3f62039
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-05-19 19:43:33 +0800

    Completed histogram generating functionality close #3

--- Project/adl.py
+++ Project/adl.py
@@ -24,24 +24,50 @@ print(len(df.index))
 df = df.ix[(df['door_contact_as'] == 'No') & (df['living_room_as'] == 'No') 
                    & (df['bedroom_as'] == 'No') & (df['bed_as'] == 'No') 
                    & (df['bathroom_as'] == 'No') & (df['kitchen_as'] == 'No')]
-
-
-#testing code; remove later: print size of 
-print(len(df.index))
+#reset index
 df = df.reset_index(drop=True)
-time_dict = {} 
 
-for index, row in enumerate(df.itertuples()):
-    startTimeIndex = index if index == 0 else startTimeIndex
-    timestamp = row[2]
-    if index + 1 < len(df.index):
-        #check if current timestamp and next timestamp are <= 10 second time difference
-        if (df.at[index + 1, 'date'] - timestamp).total_seconds() > 10:
-            startTime = df.at[startTimeIndex,'date']
-            time_dict[str(startTime)] = (timestamp - startTime).total_seconds()
-            startTimeIndex = index + 1
+#get difference between rows
+df['TimeDelta'] = pd.TimedeltaIndex(df['date'].diff().fillna(0))
+
+# print(df)
+time_dict = []
+startTimeIndex = None
+total_sec = 0
+for row in df.itertuples():
+    startTimeIndex = row[0] if startTimeIndex == None else startTimeIndex
+    tdelta = row[9]
+    if(row[0] + 1 < len(df.index)):
+        if(df.at[row[0] + 1, 'TimeDelta'].total_seconds() > 10):
+            startTime = df.at[startTimeIndex, 'date']
+            time_dict.append((startTime,total_sec + tdelta.total_seconds()))
+            total_sec = 0
+            startTimeIndex = row[0] + 1
+        else:
+            total_sec += tdelta.total_seconds()
+
+#just itertuples also 12+ seconds
+# startTimeIndex = None
+# for row in df.itertuples():
     
-new_df = pd.DataFrame.from_dict(time_dict, orient='index')
+#     startTimeIndex = row[0] if startTimeIndex == None else startTimeIndex
+#     timestamp = row[2]
     
+#     if (row[0] + 1 < len(df.index)):
+#         if (df.at[row[0] + 1, 'date'] - timestamp).total_seconds() > 10:
+#             startTime = df.at[startTimeIndex,'date']
+#             time_dict.append((startTime , (timestamp - startTime).total_seconds()))
+#             startTimeIndex = row[0] + 1
+# new_df = pd.Series(time_dict)
+                             
+new_df = pd.DataFrame(time_dict, columns=['timestamp','duration'])
 print(new_df.head())
+print(new_df.describe())
+binwidth = 500
+new_df.plot.hist(bins=np.arange(min(new_df['duration']), max(new_df['duration']) + binwidth, binwidth))
+# new_df.to_csv('test.csv')
+
+
 print("Elasped Time: ", round(time.time() - time_now, 3), "seconds")
+
+
