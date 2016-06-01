5065c5f0493a6d92496bb85fb91b5c85161b5dea
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-06-02 01:35:14 +0800

    Updated code, exploring tensor flow library

--- Project/adl.py
+++ Project/adl.py
@@ -4,14 +4,21 @@ EMAIL	: amos.tan.2014@sis.smu.edu.sg
 DATE    : 
 
 """
+
+############################################################################################
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from sklearn.cluster import DBSCAN
+############################################################################################
 
 time_now = time.time()
 #function to read csv
 def read(filename='../Workspace/DATA/SensorReading_2015-10_S001.csv'):
+    """
+    Creates and return a pandas.DataFrame object representation of the csv file.
+    """
     df = pd.read_csv(filename, delimiter=',', usecols=[x for x in range(0,8)],parse_dates=[1])
     return df
 
@@ -32,9 +39,9 @@ time_delta = df['TimeDelta']
 print(list(df))
 time_stamp = df['date']
 
-
-print(type(time_delta))
-print(type(time_stamp))
+#for testing, remove.
+# print(type(time_delta))
+# print(type(time_stamp))
 
 time_dict = []
 some_time = time.time()
@@ -52,7 +59,26 @@ for timestamp, delta in zip(time_stamp, time_delta):
         
 new_df = pd.DataFrame(time_dict, columns=['timestamp','duration'])
 
+# print(type(new_df['timestamp']))
+print(new_df.head(), '\n')
+
+# new_df['timestamp'] = new_df['timestamp'].map(pd.Timestamp.time)
+# new_df['timestamp'] = new_df['timestamp'].apply(lambda x: x.strftime('%H:%M:%S'))
+# new_df['timestamp'] = new_df['timestamp'].apply(lambda x: pd.to_datetime(x, ))
+
+#convert timestamp to unix time, in float
+# new_df['timestamp'] = new_df['timestamp'].astype(np.int64)
 print(new_df.head())
 print()
 print(new_df.describe())
+
+# nd_array = new_df.as_matrix()
+# print(len(nd_array), nd_array[0:5])
+# db = DBSCAN().fit(nd_array)
+print()
+# print(db, '\n')
+# print(len(set(db.labels_)) - (1 if -1 in db.labels_ else 0))
+
+
+
 print("Elasped Time: ", round(time.time() - time_now, 3), "seconds")
