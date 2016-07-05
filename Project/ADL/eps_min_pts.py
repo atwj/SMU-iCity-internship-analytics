adab22a95c38dc38f96637625286dacc1330f3d7
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-07-05 19:14:29 +0800

    updated notebooks

---
+++ Project/ADL/eps_min_pts.py
@@ -0,0 +1,12 @@
+"""
+Calculate eps & minPts
+
+"""
+################ IMPORTS ##################
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from math import pi
+from sklearn.cluster import DBSCAN
+################ IMPORTS ##################
