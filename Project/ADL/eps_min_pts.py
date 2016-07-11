ec39beb91b60d7b48d60c3fd6f4027d1c08b69eb
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-07-12 00:27:47 +0800

    exploring knee finding algo for mints

--- Project/ADL/eps_min_pts.py
+++ Project/ADL/eps_min_pts.py
@@ -7,6 +7,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from math import pi
 from sklearn.cluster import DBSCAN
 ################ IMPORTS ##################
+
+# TODO:
+# 1. Significance of similarity in the data????????????????
+
+# Using the 'knee' method of choosing an eps and min pts
+def knee_calculate_eps_minPts(distance_matrix):
+	# Flatten matrix
+	X = distance_matrix.flatten()
+	print(X)
+	
+	# Get non-zeros
+	X_non_zeros_ = X.nonzero()
+	# Mask out non-zeroes
+	X_array = X[X_non_zeros_]
+	# Generate histogram
+	hist, bin_edges = np.histogram(X_array, density=False)
+	# Arbitrarily select the knee, occasionally it falls in the 2nd or 3rd bin.
+	#TODO: Use vector projection to select knee point.
+	eps = bin_edges[1]
+
+
