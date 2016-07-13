a00fd0826716dbd7837d3b22bfde1f224e04d121
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-07-13 23:50:57 +0800

    Added new cluster metrics script

---
+++ Project/ADL/cluster_metrics.py
@@ -0,0 +1,13 @@
+'''
+Cluster metrics module.
+
+
+'''
+
+################################
+import numpy as np
+import pandas as pd
+
+################################
+
+#TODO: metrics for clusters... 
