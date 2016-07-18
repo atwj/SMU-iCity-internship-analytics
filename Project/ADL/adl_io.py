fb3de02af56c1dc0ada32382fbb4514be2fec217
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-07-18 18:19:43 +0800

    Processed aggregated files, 503 successful

--- Project/ADL/adl_io.py
+++ Project/ADL/adl_io.py
@@ -1,3 +1,31 @@
 """
 file writer
 """
+
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import numpy as np
+import cluster_sleep_periods as csp
+
+def sleep_to_cluster_aggregate(path,output_path):
+
+	files = [f for f in listdir(path) if isfile(join(path, f))]
+	file_count = len(files)
+	files_with_error = []
+	for f in files:
+		month = f[16:23]
+		id = f[24:28]
+		try:
+			output_dict = csp.return_clusters(path + f)
+			output_dict['id'] = id
+			output_dict['month'] = month
+			df = pd.DataFrame(output_dict)
+			print(df.columns)
+		except ValueError:
+			file_count -= 1
+			files_with_error.append(f)
+	print('Files parsed successfully ', file_count)
+	print('Files with errors: ', files_with_error)
+
+sleep_to_cluster_aggregate('sleep/sleep_agg/', 'sleep/sleep_agg_results')
