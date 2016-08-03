46e9560c8436e95aa6819c783bdbfd302d154159
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-08-03 16:58:02 +0800

    Added documentation for bdedbscan.py

--- Project/ADL/bdedbscan.py
+++ Project/ADL/bdedbscan.py
@@ -1,3 +1,10 @@
+"""
+AUTHOR	: TAN WEI JIE AMOS
+EMAIL	: amos.tan.2014@sis.smu.edu.sg
+DATE    : 03-08-2016
+TITLE   : bdedbscan.py
+"""
+
 '''
 This is the implementation of the BDE-DBSCAN algorithm from the following journal,
 
@@ -6,13 +13,6 @@ Choosing DBSCAN Parameters Automatically using Differential Evolution.
 International Journal of Computer Applications IJCA, 91(7), 
 1-11. doi:10.5120/15890-5059
 
+To be implemented
 '''
-
-def bde_dbscan(dataset, i_matrix):
-	maxIter = 500
-	nPop = 500
-	# nVar = 
-
-	bestSol = []
-
 	
